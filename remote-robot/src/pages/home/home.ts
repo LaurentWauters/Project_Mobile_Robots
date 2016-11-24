d6bbe07d02b68afdbdeadde840bc25f9dccd287e
Skagoo <dhr.vandenwyngaert@outlook.com>
2016-11-24 14:17:53 +0100

    Login Fix + BARM stuff

--- remote-robot/src/pages/home/home.ts
+++ remote-robot/src/pages/home/home.ts
@@ -9,24 +9,32 @@ import { RobotService } from '../../providers/robot-service';
 })
 export class HomePage {
 
-	loginSuccess: Boolean
-	private robotService: RobotService
+	loginSuccess: Boolean;
+	robotService: RobotService;
 	ip = {
 		adress: ''
 	};
 
-  constructor(public navCtrl: NavController, robotService: RobotService) {
-		this.robotService = robotService;
+  constructor(public navCtrl: NavController, private myRobotService: RobotService) {
+		this.robotService = myRobotService;
 
-		robotService.getRobot().subscribe(response => {
-			console.log("GET_ROBOT RESPONSE: " + response.toString());
-		});
+		// robotService.getRobot().subscribe(response => {
+		// 	console.log("GET_ROBOT RESPONSE: " + response.toString());
+		// });
 	}
 
 	loginForm(form) {
-    console.log(form.value.adress)
+    console.log(form.value.adress);
+		// this.robotService.login(form.value.adress);
+
 		this.robotService.login(form.value.adress).subscribe(response => {
 			console.log(response)
-		});
+			if(response.status == 200) {
+				this.loginSuccess = true;
+			}
+			else {
+				this.loginSuccess = false;
+			}	
+		})
 	}
 }
