16c2427f174e73a63838c5f6b883ba5ac42387f6
Skagoo <dhr.vandenwyngaert@outlook.com>
2016-11-28 12:48:28 +0100

    He said that he can now say the say is implemented, just saying

--- remote-robot/src/pages/say/say.ts
+++ remote-robot/src/pages/say/say.ts
@@ -1,15 +1,40 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { RobotService } from '../../providers/robot-service';
+import { ShareService } from '../services/ShareService';
+
 @Component({
   selector: 'page-say',
   templateUrl: 'say.html'
 })
 export class SayPage {
 
-  constructor(public navCtrl: NavController) {}
+  robotService: RobotService;
+  shareService: ShareService;
+
+  responses: String[];
+
+  constructor(public navCtrl: NavController, private myShareService: ShareService, private myRobotService: RobotService) {
+    this.robotService = myRobotService;
+    this.shareService = myShareService;
+    this.responses = new Array();
+  }
 
   ionViewDidLoad() {
     console.log('Hello Say Page');
+  }
+
+  doSpeak(form) {
+    this.robotService.doSpeak(form.value.text).subscribe(response => {
+      console.log(response)
+      if (response.status == 200) {
+        console.log("performing say success");
+        this.responses.push(response);
+      }
+      else {
+        console.log("performing say failed")
+      }
+    })
   }
 }
